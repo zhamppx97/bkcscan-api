a95698d1d52c2dc1f3a572fa6f1f6049a53f9664
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2021-06-04 17:05:38 +0700

    Add account balance multi

---
+++ src/account_balancemulti.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	MODULE := "account"
+	ACTION := "balancemulti"
+	ADDRESS := "0x107F4dC628EE75EbcB50953f8a72EbddD280869C"
+
+	url := "https://bkcscan.com/api?module=" + MODULE + "&action=" + ACTION + "&address=" + ADDRESS
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
